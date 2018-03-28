520aa93f13ddd55f5337d9a0c6f6edeecbb5a091
Gustavo Gonzalez <gusgon1@yahoo.com>
2018-03-27 23:00:03 -0500

    [WIP] Adding stuff to verify email

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,2 +1,10 @@
 class ApiController < ActionController::API
+  def email_lookup
+    p = Person.find_by(email: params[:email])
+    if p
+      render json: p
+    else
+      render json: { new_user: true }
+    end
+  end
 end
