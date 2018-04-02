1de4aaf612e4527789acc7e55883d960abce56d4
Gustavo Gonzalez <gusgon1@yahoo.com>
2018-04-01 21:00:17 -0500

    [WIP] Fixing routes and allowing any source for cors

---
+++ app/controllers/api/v1/api_controller.rb
@@ -0,0 +1,12 @@
+module Api::V1
+  class ApiController < ActionController::API
+    def email_lookup
+      p = Person.find_by(email: params[:email])
+      if p
+        render json: p
+      else
+        render json: { new_user: true }
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      get :email_lookup, to: 'api#email_lookup'
+    end
+  end
 end
