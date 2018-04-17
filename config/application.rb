73ab2b5474c59224206c357c55e6ee57ff6cb58b
Gustavo Gonzalez <gusgon1@yahoo.com>
2018-04-17 16:31:38 -0500

    Try to fix issue with locales active admin and production

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,8 @@ module FcrColectaApi
 		config.middleware.use ActionDispatch::Session::CookieStore
 
     config.action_mailer.default_url_options = { host: Rails.application.secrets.ui_url }
+
+    I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.yml').to_s]
     config.i18n.default_locale = :es
   end
 end
