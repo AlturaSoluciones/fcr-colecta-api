5def21fcc1fd0925590ed202bafc0e480476b184
Alberto Aragon <alberto@alturasoluciones.com>
2018-04-17 10:49:39 -0500

    Adding place to the list of person attributes in active admin

--- app/admin/people.rb
+++ app/admin/people.rb
@@ -1,5 +1,24 @@
 ActiveAdmin.register Person do
   permit_params :firstname, :lastname, :identifier, :birthday, :email, :gender,
                 :phone, :cellphone, :status, :invited_by_id, :confirmation_token,
-                :confirmed_at
+                :confirmed_at, :location
+
+  index do
+    selectable_column
+    column "Firstname", :firstname
+    column "Lastname", :lastname
+    column "Identifier", :identifier
+    column "Birthday", :birthday
+    column "Email", :email
+    column "Gender", :gender
+    column "Phone", :phone
+    column "Cellphone", :cellphone
+    column "Status", :status
+    column "Invited by", :invited_by_id
+    column "Confirmation token", :confirmation_token
+    column "Confirmed at", :confirmed_at
+    column :place do |p|
+      p.location ? p.location.place.name : "No place confirmed yet"
+    end
+  end
 end
