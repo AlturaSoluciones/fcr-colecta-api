1ce9df785e49baf45315ba4e87b8df215e236ff4
Gustavo Gonzalez <gusgon1@yahoo.com>
2018-04-30 13:46:51 -0500

    Fix condition for all day

--- app/models/place.rb
+++ app/models/place.rb
@@ -4,11 +4,13 @@ class Place < ApplicationRecord
 
   def self.available(schedule_id)
     schedule = Schedule.find(schedule_id)
+    kinds = (schedule.kind == 'all' ? ['am', 'pm'] : ['all', schedule.kind])
+              .map{|k| connection.quote k}.join(', ')
     where(status: :active)
       .joins("LEFT JOIN (locations INNER JOIN
                  schedules ON locations.schedule_id = schedules.id
                  AND schedules.day = #{connection.quote schedule.day}
-                 AND schedules.kind IN ('all', #{connection.quote schedule.kind}))
+                 AND schedules.kind IN (#{kinds}))
                  ON places.id = locations.place_id ")
       .where(locations: { id: nil }).distinct
   rescue Exception => e
