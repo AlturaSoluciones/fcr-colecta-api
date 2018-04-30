45504aeb4021a79af977739fac9725aa27c746c6
Gustavo Gonzalez <gusgon1@yahoo.com>
2018-04-30 14:11:50 -0500

    Add exception notifications

---
+++ config/initializers/exception_notification.rb
@@ -0,0 +1,19 @@
+require 'exception_notification/rails'
+
+ExceptionNotification.configure do |config|
+  # Adds a condition to decide when an exception must be ignored or not.
+  # The ignore_if method can be invoked multiple times to add extra conditions.
+  config.ignore_if do |_exception, _options|
+    Rails.env.development?
+  end
+
+  # Notifiers =================================================================
+
+  # Email notifier sends notifications by email.
+  config.add_notifier :slack,
+                      webhook_url: Rails.application.secrets.slack[:url],
+                      channel: '#exceptions',
+                      additional_parameters: {
+                        mrkdwn: true
+                      }
+end
