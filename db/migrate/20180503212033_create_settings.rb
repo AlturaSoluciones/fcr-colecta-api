62a205fa086ce61d69a5a6a89ae285afbd444933
Alberto Aragon <alberto@alturasoluciones.com>
2018-05-03 17:32:42 -0500

    Validating fields presence and not null values

--- db/migrate/20180503212033_create_settings.rb
+++ db/migrate/20180503212033_create_settings.rb
@@ -1,8 +1,8 @@
 class CreateSettings < ActiveRecord::Migration[5.1]
   def change
     create_table :settings do |t|
-      t.string :name
-      t.string :value
+      t.string :name, null: false
+      t.string :value, null: false
 
       t.timestamps
     end
