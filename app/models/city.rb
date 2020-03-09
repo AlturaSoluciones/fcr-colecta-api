362e262d285db771935edb98357f0ee4756f3811
Gustavo Gonzalez <gusgon1@yahoo.com>
2020-03-09 13:28:17 -0500

    Set right scope

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,5 @@ class City < ApplicationRecord
   has_many :places
   has_many :locations, through: :places
 
-  scope :active, -> { where(status: :active) }
+  scope :active, -> { where(active: true) }
 end
